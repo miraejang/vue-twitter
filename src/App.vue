be1a0faffb1c3eaba959bc3e13f6de97621282e9
miraejang <mirae611@nate.com>
2021-07-05 10:35:45 +0900

    트위팅 섹션

--- src/App.vue
+++ src/App.vue
@@ -62,7 +62,29 @@
     </div>
 
     <!-- content -->
-    <div class="flex-1"></div>
+    <div class="flex-1">
+      <div class="flex h-screen">
+        <!-- main section -->
+        <div class="flex-1 border-r border-gray-100">
+          <div class="felx flex-col">
+            <!-- page title -->
+            <div class="border-b border-gray-100 px-3 py-2 font-bold text-lg">홈</div>
+            <!-- tweeting section -->
+            <div class="flex p-3 border-b-8 border-gray-100">
+              <img src="https://picsum.photos/200" class="w-10 h-10 mr-2 rounded-full hover:opacity-70 cursor-pointer" alt="">
+              <div class="flex flex-1 flex-col">
+                <textarea class="w-full text-lg font-bold focus:outline-none resize-none" placeholder="무슨 일이 일어나고 있나요?"></textarea>
+                <div class="text-right">
+                  <button class="bg-primary hover:bg-dark text-sm font-bold text-white mt-3 px-4 py-2 rounded-full">트윗</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- trend section -->
+        <div class="w-2/5">trend</div>
+      </div>
+    </div>
   </div>
 </template>
 

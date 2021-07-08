2138ebbd18c2b57bc6ea7bbb9eae68715c311e91
miraejang <mirae611@nate.com>
2021-07-08 14:48:12 +0900

    slide menu를 routes를 반복하여 사용

--- src/App.vue
+++ src/App.vue
@@ -7,39 +7,10 @@
         <i class="fab fa-twitter text-3xl text-primary ml-4 mb-3"></i>
         <!-- menu -->
         <div class="flex flex-col items-start space-y-1">
-          <router-link to="/" class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
-            <i class="fas fa-home fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden xl:inline-block">홈</span>
+          <router-link to="/" class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer" v-for="route in routes" :key="route">
+            <i class="text-2xl" :class="route.icon"></i>
+            <span class="ml-3 text-lg hidden xl:inline-block">{{ route.title }}</span>
           </router-link>
-          <router-link to="/" class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
-            <i class="fas fa-hashtag fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden xl:inline-block">탐색하기</span>
-          </router-link>
-          <router-link to="/notifications" class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
-            <i class="far fa-bell fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden xl:inline-block">알림</span>
-          </router-link>
-          <router-link to="/messages" class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
-            <i class="far fa-envelope fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden xl:inline-block">쪽지</span>
-          </router-link>
-          <router-link to="/" class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
-            <i class="far fa-bookmark fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden xl:inline-block">북마크</span>
-          </router-link>
-          <router-link to="/" class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
-            <i class="far fa-list-alt fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden xl:inline-block">리스트</span>
-          </router-link>
-          <router-link to="/profile" class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
-            <i class="far fa-user fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden xl:inline-block">프로필</span>
-          </router-link>
-          <router-link to="/" class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
-            <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden xl:inline-block">더보기</span>
-          </router-link>
-          <ul></ul>
         </div>
         <div class="mt-3 w-full flex justify-center">
           <button class="bg-primary text-white xl:w-full w-12 h-12 rounded-full hover:bg-dark">
@@ -62,24 +33,26 @@
     </div>
 
     <!-- content -->
-    <!-- <twitter-home></twitter-home> -->
-    <!-- <twitter-notifications></twitter-notifications> -->
-    <!-- <twitter-messages></twitter-messages> -->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import TwitterHome from "./pages/home.vue";
-import TwitterNotifications from "./pages/notifications.vue";
-import TwitterMessages from "./pages/messages.vue";
-import TwitterProfile from "./pages/profile.vue";
+import { ref, onBeforeMount } from "vue";
+import router from "./router";
+
 export default {
-  components: { TwitterHome, TwitterNotifications, TwitterMessages, TwitterProfile },
-  setup() {}
-}
+  setup() {
+    const routes = ref([]);
+
+    onBeforeMount(() => {
+      routes.value = router.options.routes;
+    });
+
+    return { routes };
+  },
+};
 </script>
 
 <style>
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,14 @@ import TwitterNotifications from "../pages/notifications.vue";
 import TwitterProfile from "../pages/profile.vue";
 
 const routes = [
-  { path: '/', component: TwitterHome },
-  { path: '/messages', component: TwitterMessages },
-  { path: '/notifications', component: TwitterNotifications },
-  { path: '/profile', component: TwitterProfile },
+  { path: '/', component: TwitterHome, title: '홈', icon: 'fas fa-home fa-fw' },
+  { path: '/', component: TwitterHome, title: '탐색하기', icon: 'fas fa-hashtag fa-fw' },
+  { path: '/notifications', component: TwitterNotifications, title: '알림', icon: 'far fa-bell fa-fw' },
+  { path: '/messages', component: TwitterMessages, title: '쪽지', icon: 'far fa-envelope fa-fw' },
+  { path: '/', component: TwitterMessages, title: '북마크', icon: 'far fa-envelope fa-fw' },
+  { path: '/', component: TwitterMessages, title: '리스트', icon: 'far fa-list-alt fa-fw' },
+  { path: '/profile', component: TwitterProfile, title: '프로필', icon: 'far fa-user fa-fw' },
+  { path: '/', component: TwitterProfile, title: '더보기', icon: 'fas fa-ellipsis-h fa-fw' },
 ]
 
 const router = createRouter({

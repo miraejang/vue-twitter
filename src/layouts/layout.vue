7c52b3ed79df9fb8a9d978e81292fc5567e68ef1
miraejang <mirae611@nate.com>
2021-07-08 16:28:51 +0900

    레이아웃 분리

---
+++ src/layouts/layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <component :is="layout"></component>
+</template>
+
+<script>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import DefaultLayout from "./defaultLayout.vue";
+import EmptyLayout from "./emptyLayout.vue";
+
+export default {
+  components: { DefaultLayout, EmptyLayout },
+  setup() {
+    const route = useRoute();
+    const layout = computed(() => route.meta.layout || 'DefaultLayout' );
+
+    return { layout };
+  },
+};
+</script>
+
+<style>
+</style>

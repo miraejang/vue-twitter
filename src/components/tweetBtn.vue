41558d28c12cd370fe978f8e2afb94033a6712a3
mirae <mirae611@nate.com>
2021-07-18 23:57:56 +0900

    tweet btn 생성

---
+++ src/components/tweetBtn.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="text-right sm:hidden mr-2">
+    <button v-if="!tweetBody.length" class="bg-light text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
+    <button v-else @click="onAddTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/tweetModal.vue
+++ src/components/tweetModal.vue
@@ -7,21 +7,12 @@
       <div @click.stop class="inline-block bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg w-full">
         <div class=" flex justify-between items-center p-2 border-b border-default">
           <button @click="$emit('closeModal')" class="fas fa-times text-primary text-lg p-2 w-10 h-10 hover-bg-blue-50 rounded-full"></button>
-          <div class="text-right sm:hidden mr-2">
-            <button v-if="!tweetBody.length" class="bg-light text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
-            <button v-else @click="onAddTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
-          </div>
+          <tweet-btn></tweet-btn>
         </div>
         <div class="flex p-4">
           <!-- <img :src="currentUser.profile_image_url" class="w-10 h-10 mr-2 rounded-full hover:opacity-70 cursor-pointer" alt=""> -->
           <img src="https://picsum.photos/200" class="w-10 h-10 mr-2 rounded-full hover:opacity-70 cursor-pointer" alt="">
-          <div class="flex flex-1 flex-col">
-            <textarea v-model="tweetBody" rows="5" class="w-full text-lg font-bold focus:outline-none resize-none" placeholder="무슨 일이 일어나고 있나요?"></textarea>
-            <div class="text-right hidden sm:block">
-              <button v-if="!tweetBody.length" class="bg-light text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
-              <button v-else @click="onAddTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
-            </div>
-          </div>
+          <tweet-btn></tweet-btn>
         </div>
       </div>
     </div>
@@ -30,8 +21,10 @@
 
 <script>
 import { ref } from "vue";
+import TweetBtn from "/src/components/tweetBtn.vue";
 
 export default {
+  components: [ ' TweetBtn' ],
   setup() {
     const tweetBody = ref('')
 

8b2fb03ca0262c76ea89ad0c069394bf9911826b
miraejang <mirae611@nate.com>
2021-08-03 12:09:17 +0900

    comment 기능 추가

--- src/components/commentModal.vue
+++ src/components/commentModal.vue
@@ -9,7 +9,7 @@
           <button @click="$emit('closeModal')" class="fas fa-times text-primary text-lg p-2 w-10 h-10 hover-bg-blue-50 rounded-full"></button>
           <div class="text-right sm:hidden mr-2">
             <button v-if="!tweetBody.length" class="bg-light text-sm font-bold text-white px-4 py-1 rounded-full">Reply</button>
-            <button v-else @click="onAddTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">Reply</button>
+            <button v-else @click="onCommentTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">Reply</button>
           </div>
         </div>
         <div>
@@ -39,7 +39,7 @@
               <textarea v-model="tweetBody" rows="5" class="w-full text-lg font-bold focus:outline-none resize-none" placeholder="Tweet yout reply"></textarea>
               <div class="text-right hidden sm:block">
                 <button v-if="!tweetBody.length" class="bg-light text-sm font-bold text-white px-4 py-1 rounded-full">Reply</button>
-                <button v-else @click="onAddTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">Reply</button>
+                <button v-else @click="onCommentTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">Reply</button>
               </div>
             </div>
           </div>
@@ -53,14 +53,36 @@
 import { ref, computed } from "vue";
 import moment from "moment";
 import store from "/src/store";
+import firebase from "firebase";
+import { COMMENT_COLLECTION, TWEET_COLLECTION } from "/src/firebase";
 
 export default {
   props: ['tweet'],
-  setup() {
+  setup(props, {emit}) {
     const tweetBody = ref('')
     const currentUser = computed(() => store.state.user)
 
-    return { tweetBody, moment, currentUser }
+    const onCommentTweet = async () => {
+      try {
+        const doc = COMMENT_COLLECTION.doc();
+        await doc.set({
+          id: doc.id,
+          from_tweet_id: props.tweet.id,
+          comment_tweet_body: tweetBody.value,
+          uid: currentUser.value.uid,
+          created_at: Date.now(),
+        })
+
+        await TWEET_COLLECTION.doc(props.tweet.id).update({
+          num_commnets: firebase.firestore.FieldValue.increment(1)
+        })
+        emit('close-modal')
+      } catch (e) {
+        console.log("on comment tweet error", e);
+      }
+    }
+
+    return { tweetBody, moment, currentUser, onCommentTweet }
   },
 };
 </script>
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -19,3 +19,4 @@ export const storage = firebase.storage()
 
 export const USER_COLLECTION = db.collection("users")
 export const TWEET_COLLECTION = db.collection("tweets")
+export const COMMENT_COLLECTION = db.collection("comments")

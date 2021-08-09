f7ef6b9ae7e7d1df9a53cf7bd976460a04600828
miraejang <mirae611@nate.com>
2021-08-09 16:00:48 +0900

    profile page in tabs view

--- src/views/profile.vue
+++ src/views/profile.vue
@@ -32,21 +32,21 @@
         </div>
         <div>
           <span class="font-bold mr-1">{{ currentUser.followings.length }}</span>
-          <span class="text-light mr-3">팔로우 중</span>
+          <span class="text-light mr-3">Follwing</span>
           <span class="font-bold mr-1">{{ currentUser.followers.length }}</span>
-          <span class="text-light">팔로워</span>
+          <span class="text-light">Follwers</span>
         </div>
       </div>
       <!-- tabs -->
       <div class="flex justify-between border-b border-default mt-3">
-        <div class="w-1/4 py-3 border-b border-primary font-bold text-primary text-center hover:bg-blue-50 hover:text-primary cursor-pointer">트윗</div>
-        <div class="w-1/4 py-3 border-b border-primary font-bold text-light text-center hover:bg-blue-50 hover:text-primary cursor-pointer">트윗 및 답글</div>
-        <div class="w-1/4 py-3 border-b border-primary font-bold text-light text-center hover:bg-blue-50 hover:text-primary cursor-pointer">미디어</div>
-        <div class="w-1/4 py-3 border-b border-primary font-bold text-light text-center hover:bg-blue-50 hover:text-primary cursor-pointer">마음에 들어요</div>
+        <div @click="currentTab = 'tweets'" :class="currentTab === 'tweets' ? 'border-b border-primary text-primary' : 'text-light'" class="w-1/4 py-3 font-bold text-center hover:bg-blue-50 hover:text-primary cursor-pointer">Tweets</div>
+        <div @click="currentTab = 'replies'" :class="currentTab === 'replies' ? 'border-b border-primary text-primary' : 'text-light'" class="w-1/4 py-3 font-bold text-center hover:bg-blue-50 hover:text-primary cursor-pointer">Tweets & repiles</div>
+        <div @click="currentTab = 'media'" :class="currentTab === 'media' ? 'border-b border-primary text-primary' : 'text-light'" class="w-1/4 py-3 font-bold text-center hover:bg-blue-50 hover:text-primary cursor-pointer">Media</div>
+        <div @click="currentTab = 'likes'" :class="currentTab === 'likes' ? 'border-b border-primary text-primary' : 'text-light'" class="w-1/4 py-3 font-bold text-center hover:bg-blue-50 hover:text-primary cursor-pointer">Likes</div>
       </div>
       <!-- tweets -->
       <div class="overflow-y-scroll">
-        <tweet-form v-for="tweet in tweets" :key="tweet.id" :currentUser="currentUser" :tweet="tweet"></tweet-form>
+        <tweet-form v-for="tweet in currentTab === 'tweets' ? tweets : currentTab === 'replies' ? replies : likes" :key="tweet.id" :currentUser="currentUser" :tweet="tweet"></tweet-form>
       </div>
     </div>
     <!-- trend section -->
@@ -59,7 +59,7 @@ import { computed, ref, onBeforeMount } from '@vue/runtime-core'
 import TrendSection from "../components/trends.vue"
 import TweetForm from "../components/tweet.vue"
 import store from '../store'
-import { TWEET_COLLECTION, USER_COLLECTION } from '../firebase'
+import { LIKES_COLLECTION, RETWEET_COLLECTION, TWEET_COLLECTION, USER_COLLECTION } from '../firebase'
 import getTweetInfo from '../untils/getTweetInfo'
 import moment from 'moment'
 
@@ -68,6 +68,11 @@ export default {
   setup() {
     const currentUser = computed(() => store.state.user)
     const tweets = ref([])
+    const replies = ref([])
+    const media = ref([])
+    const likes = ref([])
+    const currentTab = ref('tweets')
+
     onBeforeMount(() => {
       USER_COLLECTION.doc(currentUser.value.uid).onSnapshot(doc => {
         store.commit('SET_USER', doc.data())
@@ -89,9 +94,45 @@ export default {
             }
           })
         })
+
+      RETWEET_COLLECTION
+        .where('uid', '==', currentUser.value.uid)
+        .orderBy('created_at', 'desc')
+        .onSnapshot(snapshot => {
+          snapshot.docChanges().forEach(async (change) => {
+            const doc = await TWEET_COLLECTION.doc(change.doc.data().from_tweet_id).get()
+            let tweet = await getTweetInfo(doc.data(), currentUser.value)
+            
+            if (change.type === 'added') {
+              replies.value.splice(change.newIndex, 0, tweet)
+            } else if (change.type === 'modified') {
+              replies.value.splice(change.oldIndex, 1, tweet)
+            } else if (change.type === 'removed') {
+              replies.value.splice(change.oldIndex, 1)
+            }
+          })
+        })
+
+      LIKES_COLLECTION
+        .where('uid', '==', currentUser.value.uid)
+        .orderBy('created_at', 'desc')
+        .onSnapshot(snapshot => {
+          snapshot.docChanges().forEach(async (change) => {
+            const doc = await TWEET_COLLECTION.doc(change.doc.data().from_tweet_id).get()
+            let tweet = await getTweetInfo(doc.data(), currentUser.value)
+            
+            if (change.type === 'added') {
+              likes.value.splice(change.newIndex, 0, tweet)
+            } else if (change.type === 'modified') {
+              likes.value.splice(change.oldIndex, 1, tweet)
+            } else if (change.type === 'removed') {
+              likes.value.splice(change.oldIndex, 1)
+            }
+          })
+        })
     })
 
-    return { currentUser, tweets, moment }
+    return { currentUser, tweets, replies, media, likes, moment, currentTab }
   }
 }
 </script>
